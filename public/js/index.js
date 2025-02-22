9e828b5b2a05635101f80175ea0e04aeb1cbe6d3
javi-pg <jesperanza.pg@gmail.com>
2025-02-22 08:56:12 -0300

    arreglos

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
-//iniciamos la conexion desde nuestro cliente 
 const socket = io();
+const form = document.getElementById("product-form");
 
-//emitimos un evento desde el cliente 
-socket.emit("chat message", "Hola saludos desde el cliente web");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const product = {
+        title: document.getElementById("title").value,
+        price: parseFloat(document.getElementById("price").value),
+        description: document.getElementById("description").value,
+        code: document.getElementById("code").value,
+        stock: parseInt(document.getElementById("stock").value),
+        category: document.getElementById("category").value,
+        status: true
+    };
+
+    socket.emit("newProduct", product);
+
+    form.reset();
+});
